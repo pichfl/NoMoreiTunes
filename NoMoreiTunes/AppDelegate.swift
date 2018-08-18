c2bde79fb3e547173fcc1a3eeb81384f773d8843
Florian Pichler <f.pichler@anfe.ma>
2018-08-18 20:01:29 +0200

    Initial Commit

---
+++ NoMoreiTunes/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  NoMoreiTunes
+//
+//  Created by Florian Pichler on 18.08.18.
+//  Copyright © 2018 YLKGD. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ NoMoreiTunes/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  NoMoreiTunes
+//
+//  Created by Florian Pichler on 18.08.18.
+//  Copyright © 2018 YLKGD. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
